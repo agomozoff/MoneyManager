f02b04a8182c2b327b1d720feccf49f2ea636800
Antonio <antony1o@yandex.ru>
2020-04-21 12:03:47 +0400

    add method Object, delete functions

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ let money,
     expenseItem,
     expenseItemValue;
 
+function start() {
+    money = +prompt('Ваш бюджет на месяц:');
+    time = prompt('Введите дату в формате YYYY-MM-DD:', 'YYYY-MM-DD');
 
-money = +prompt('Ваш бюджет на месяц:');
-time = prompt('Введите дату в формате YYYY-MM-DD:', 'YYYY-MM-DD');
+    if(isNaN(money) || money == "" || money == null) {
+        money = +prompt('Вы ввели некорректные данные! Введите ваш бюджет на месяц:');
+    }
+}
+start();
+
 
 let appData = {
     budget: money,
@@ -17,68 +24,117 @@ let appData = {
         
     },
     income: [],
-    savings: false
-};
-
-for (i = 0; i < 2; i++) {
-    expenseItem = prompt('Введите обязательную статью расходов в этом месяце:');
-
-    if (typeof(expenseItem) === "string" && typeof(expenseItem) != null && expenseItem != "") {
-
-        expenseItemValue = +prompt('Во сколько рублей обойдётся?');
-
-        if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
-            appData.expenses[expenseItem] = expenseItemValue;
-        } else {
-            let isCorrect = false;
-
-            while (isCorrect == false) {
-                expenseItemValue = +prompt('Вы допустили ошибку при вводе! Укажите во сколько рублей обойдётся покупка?');
-                if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
-                    isCorrect = true;
-                }
-            }
-            appData.expenses[expenseItem] = expenseItemValue;
-        }
-    } else {
-        let isCorrect = false;
-
-        while (isCorrect == false) {
-            expenseItem = prompt('Вы допустили ошибку при вводе! Введите обязательную статью расходов в этом месяце заново:');
+    savings: true,
+    chooseExpenses: function() {
+        for (i = 0; i < 2; i++) {    
+            expenseItem = prompt('Введите обязательную статью расходов в этом месяце:');
             if (typeof(expenseItem) === "string" && typeof(expenseItem) != null && expenseItem != "") {
-                isCorrect = true;
-            }
-        }
-
-        expenseItemValue = +prompt('Во сколько рублей обойдётся?');
-
-        if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
-            appData.expenses[expenseItem] = expenseItemValue;
-        } else {
-            let isCorrect = false;
-
-            while (isCorrect == false) {
-                expenseItemValue = +prompt('Вы допустили ошибку при вводе! Укажите во сколько рублей обойдётся покупка?');
+        
+                expenseItemValue = +prompt('Во сколько рублей обойдётся?');
                 if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
-                    isCorrect = true;
+        
+                    appData.expenses[expenseItem] = expenseItemValue;
+                } else {
+                    let isCorrect = false;
+        
+                    while (isCorrect == false) {
+                        expenseItemValue = +prompt('Вы допустили ошибку при вводе! Укажите во сколько рублей обойдётся покупка?');
+                        if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
+                            isCorrect = true;
+                        }
+                    }
+                    appData.expenses[expenseItem] = expenseItemValue;
+                }
+            } else {
+                let isCorrect = false;
+        
+                while (isCorrect == false) {
+                    expenseItem = prompt('Вы допустили ошибку при вводе! Введите обязательную статью расходов в этом месяце заново:');
+                    if (typeof(expenseItem) === "string" && typeof(expenseItem) != null && expenseItem != "") {
+                        isCorrect = true;
+                    };
+                };
+        
+                expenseItemValue = +prompt('Во сколько рублей обойдётся?');
+        
+                if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
+        
+                    appData.expenses[expenseItem] = expenseItemValue;
+                } else {
+                    let isCorrect = false;
+        
+                    while (isCorrect == false) {
+                        expenseItemValue = +prompt('Вы допустили ошибку при вводе! Укажите во сколько рублей обойдётся покупка?');
+                        if (isNaN(expenseItemValue) != true && typeof(expenseItemValue) != null && expenseItemValue != "") {
+                            isCorrect = true;
+                        }
+                    }
+                    appData.expenses[expenseItem] = expenseItemValue;
+                };
+            };
+        };
+    },
+
+    detectDayBudget: function() {
+        appData["moneyPerDay"] = +(appData["budget"] / 30).toFixed(1);
+        alert(`Ваш бюджет на 1 день составит: ${appData.moneyPerDay} руб.`);
+    },
+
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("Очень маленький достаток");
+        } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+            console.log("Средний достаток");
+        } else if (appData.moneyPerDay >= 2000) {
+            console.log("Очень хороший достаток")
+        };
+    },
+
+    checkSavings: function() {
+        if (appData.savings == true) {
+            let save = +prompt("Сумма ваших накоплений равна: "),
+                percent = +prompt("Под какой процент?");
+    
+            appData.monthIncome = save * percent / 100 / 12;
+            alert(`Доход с вашего депозита составляет ${appData.monthIncome} руб./месяц`);
+        };
+    },
+
+    chooseOptExpenses: function() {
+        for (i = 1; i <= 3; i++) {
+            appData.optionalExpenses[i] = prompt("Статья необязательных расходов: ");
+        };
+    },
+
+    chooseIncome: function() {
+        let items = prompt("Что принесёт вам дополнительный доход? (Перечислите через запятую)");
+        if(typeof(items) === "string" && typeof(items) != null && items != "") {
+            appData.income = items.split(", ");
+        } else {
+            isCorrect = true;
+            while(isCorrect) {
+                items = prompt("Вы неверно ввели данные! Введите через запятую информацию о дополнительном доходе");
+                if(typeof(items) === "string" && typeof(items) != null && items != "") {
+                    appData.income = items.split(", ");
+                    isCorrect = false;
                 }
             }
-            appData.expenses[expenseItem] = expenseItemValue;
         }
+        
+        appData.income.push(prompt("Может что-то ещё?"));
+        appData.income.sort();
 
+        appData.income.forEach(function(item, i) {
+            alert(`Способы дополнительного заработка: ${i + 1} - ${item}`);
+        });
+    },
+
+    appDataOutput: function() {
+        appDataKeys = "";
+        for (key in appData) {
+            appDataKeys += key + ", "; 
+        }
+        console.log(`Наша программа включает в себя данные: ${appDataKeys.slice(0, -2)}`);
     }
-    
 };
-
-appData["moneyPerDay"] = appData["budget"] / 30;
-
-alert(`Ваш бюджет на 1 день составит: ${appData.moneyPerDay} руб.`);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Очень маленький достаток");
-} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний достаток");
-} else if (appData.moneyPerDay >= 2000) {
-    console.log("Очень хороший достаток")
-}
 
